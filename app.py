44eddfe219e92465357ce06e72218c72863caedf
javi <javierrcalvete@gmail.com>
2024-08-03 03:37:14 -0300

    added to app.py test graphs

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
 import pandas as pd
+import plotly.express as px
+df = px.data.iris()
+fig = px.scatter_matrix(df, dimensions=["sepal_width", "sepal_length", "petal_width", "petal_length"], color="species")
+fig.show()
